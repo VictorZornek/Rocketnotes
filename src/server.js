f28a9f650523860f23c84f3b6f3d4c5ae352e35e
Victor Zornek <vzornek@gmail.com>
2024-08-02 16:31:17 -0300

    commit inicial do projeto com a criação da estrutura do projeto

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');  // importação do express
+
+const routes = require("./routes")
+
+
+const app = express();  // inicialização do express
+app.use(express.json());  // necessário passar o formato de json
+
+app.use(routes);
+
+const PORT = 3333;  // definir número da porta 
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+
+
+// params são utilizados para dados simples, não utilize-os em informações muito complexas
