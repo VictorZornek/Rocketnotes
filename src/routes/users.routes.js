22c4f5ad0033a5efe770893c5ca770b609d5a710
Victor Zornek <vzornek@gmail.com>
2024-08-07 14:46:27 -0300

    add metodo update

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersRoutes = Router();
 const usersController = new UsersController()  // instacia do userController
 
 usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:id", usersController.update)
 
 module.exports = usersRoutes;  // exportação para quem quiser utilizar o arquivo poder usar
