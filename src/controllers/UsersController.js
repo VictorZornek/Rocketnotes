8a79ed411365fe55806606dbf9491c92ba3b6f09
Victor Zornek <vzornek@gmail.com>
2024-08-07 13:54:54 -0300

    add INSERT do banco de dados no projeto

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -21,6 +21,9 @@ class UsersController {
             throw new AppError('Este e-mail já está em uso.');
         }
 
+        // Para realizar a inserção (create) de um usuário no banco de dados
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password]);
+
         return response.status(201).json();
     }
 }
