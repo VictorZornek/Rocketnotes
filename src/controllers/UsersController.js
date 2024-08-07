d75a5a03f5d78f16bdcb9fd9d55f17af28dd34dd
Victor Zornek <vzornek@gmail.com>
2024-08-07 15:04:53 -0300

    add update de senha criptografada no banco de dados

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs"); // importação para criptografia de senha
+const { hash, compare } = require("bcryptjs"); // importação para criptografia de senha
 const AppError = require("../utils/AppError.js");
 const sqliteConnection = require("../database/sqlite");
 
@@ -30,7 +30,7 @@ class UsersController {
     }
 
     async update(request, response) {
-        const { name, email } = request.body;
+        const { name, email, password, old_password } = request.body;
         const { id } = request.params;
 
         const database = await sqliteConnection();  // conexão com o banco de dados
@@ -49,13 +49,28 @@ class UsersController {
         user.name = name;
         user.email = email;
 
+        if(password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga para definir a nova senha!");
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if(!checkOldPassword) {
+                throw new AppError("A senha antiga não confere!");
+            }
+
+            user.password = await hash(password, 8);
+        }
+
         await database.run(`
             UPDATE users SET 
             name = ?,
             email = ?,
+            password = ?,
             update_at = ?
             WHERE id = ?`,
-            [user.name, user.email, new Date(), id]
+            [user.name, user.email, user.password, new Date(), id]
         );
 
         return response.status(200).json();
