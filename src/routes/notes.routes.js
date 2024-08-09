ca1e8d525533c20eb7a12062959b7848df70a6db
Victor Zornek <vzornek@gmail.com>
2024-08-09 15:54:04 -0300

    add function de exibir notas

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -4,8 +4,9 @@ const NotesController = require("../controllers/NotesController.js");
 
 const notesRoutes = Router();
 
-const notesController = new NotesController()  // instacia do userController
+const notesController = new NotesController();  // instacia do userController
 
-notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.get("/:id", notesController.show);
 
 module.exports = notesRoutes;  // exportação para quem quiser utilizar o arquivo poder usar
