221c7aef33ce63b33aaae392fdb7861d662e6ebe
Victor Zornek <vzornek@gmail.com>
2024-08-09 16:59:18 -0300

    add function delete no projeto

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -8,5 +8,6 @@ const notesController = new NotesController();  // instacia do userController
 
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.show);
+notesRoutes.delete("/:id", notesController.delete);
 
 module.exports = notesRoutes;  // exportação para quem quiser utilizar o arquivo poder usar
