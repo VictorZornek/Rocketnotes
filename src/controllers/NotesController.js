d2182bbf728832c3d1b7bd2f2ac3ac4bf337ae04
Victor Zornek <vzornek@gmail.com>
2024-08-10 15:29:51 -0300

    add inner join

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -56,9 +56,28 @@ class NotesController {
     }
 
     async index(request, response) {
-        const { title, user_id } = request.query;
+        const { title, user_id, tags } = request.query;
 
-        const notes = await knex("notes").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
+        let notes;
+
+        if(tags){
+            const filterTags = tags.split(',').map(tag => tag.trim());  // trim() serve para remover espaços em branco do início e do fim de uma string
+            console.log(filterTags)
+
+            notes = await knex("tags").select([
+                "notes.id",
+                "notes.title",
+                "notes.user_id"
+            ]).where("notes.user_id", user_id)
+            .whereLike("notes.title", `%${title}%`)
+            .whereIn("name", filterTags)
+            .innerJoin("notes", "notes.id", "tags.note_id")
+            .orderBy("notes.title")
+
+        } else {
+            notes = await knex("notes").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
+
+        }
 
         return response.json(notes);
     }
