0bc70e4dcd6259c2cf5e61330a3fc63dc7ef14c9
Victor Zornek <vzornek@gmail.com>
2024-09-16 18:12:22 -0300

    adicionando o token nas requisições do usuário, utilizando middleware

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
 const TagsController = require("../controllers/TagsController.js");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const tagsRoutes = Router();
 
 const tagsController = new TagsController();  // instacia do userController
 
-tagsRoutes.get("/:user_id", tagsController.index);
+tagsRoutes.get("/", ensureAuthenticated, tagsController.index);
 
 
 module.exports = tagsRoutes;  // exportação para quem quiser utilizar o arquivo poder usar
