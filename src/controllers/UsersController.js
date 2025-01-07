96d200619aced890c535c833ebac5126ce350180
Victor Zornek <vzornek@gmail.com>
2025-01-07 17:40:52 -0300

    add User Repository para criação de services de requisições do banco de dados

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -14,9 +14,6 @@ class UsersController {
     async create(request, response) {
         const { name, email, password } = request.body
 
-        const database = await sqliteConnection();
-        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])  // Para inserir variável utilize (?), [nome.variavel]
-
         if(checkUserExists) {
             throw new AppError('Este e-mail já está em uso.');
         }
@@ -24,7 +21,6 @@ class UsersController {
         const hashedPassword = await hash(password, 8);  // Para realizar a criptografia inserida pelo usuário - necessário usar await
 
         // Para realizar a inserção (create) de um usuário no banco de dados
-        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
 
         return response.status(201).json();
     }
